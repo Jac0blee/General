c2ff23446cd750b9956e3610c135db17418f4392
Jac0blee <jacobleedude@gmail.com>
2012-10-16 15:44:20 -0400

    Mock Password Checker Made

--- src/com/github/Jac0blee/HelloWorld.java
+++ src/com/github/Jac0blee/HelloWorld.java
@@ -1,11 +1,36 @@
 package com.github.Jac0blee;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public final class HelloWorld {
 
-	public static void main (String[] args) {
+	public static void main (String[] args) throws IOException {
 		
-		System.out.println("Hello World!");
-
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		String readline;
+			
+		while ((readline = in.readLine()) != null) {
+				
+			String password = "Fireworks";
+				
+			System.out.println(readline);
+			
+			if(readline.equals(password)) {
+				
+				System.out.println("Correct password.");
+				
+			}
+			else {
+				
+				System.out.println("Incorrect password");
+				
+			}
+					
+		}
+	
 	}
 
 }
