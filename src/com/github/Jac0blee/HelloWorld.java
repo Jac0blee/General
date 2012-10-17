d5db4aeb3479b90fc96a65c505c882559bd53290
Jac0blee <jacobleedude@gmail.com>
2012-10-17 13:46:46 -0400

    Mock OS (Now with help screen!)

--- src/com/github/Jac0blee/HelloWorld.java
+++ src/com/github/Jac0blee/HelloWorld.java
@@ -6,74 +6,70 @@ import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-
 public final class HelloWorld {
 
-	public static void main (String[] args) throws IOException {
-		
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		
+	public static void main(String[] args) throws IOException {
 
-	
-		        Calendar cal = Calendar.getInstance();
-		        cal.getTime();
-		        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		        
-	
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		Calendar cal = Calendar.getInstance();
+		cal.getTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
 		boolean exit = false;
-		
+
 		String readline;
-		
+
 		System.out.println("Type in the password.");
-		
+
 		while ((readline = in.readLine()) != null && exit != true) {
-				
+
 			String password = "Fireworks";
-				
+
 			System.out.println(readline);
-			
-			if(readline.equals(password)) {
-				
+
+			if (readline.equals(password)) {
+
 				System.out.println("Correct password.");
-				
-				while(!exit)
-				{
-					System.out.println("");
-					System.out.println("What do you want to do?");
-					System.out.println("1. See the time");
-					System.out.println("2. What is the OS?");
-					System.out.println("3. Exit");
+
+				System.out.println("What do you want to do?");
+				System.out.println("1. See the time");
+				System.out.println("2. What is the OS?");
+				System.out.println("3. Help");
+				System.out.println("4. Exit");
+
+				while (!exit) {
+
 					readline = in.readLine();
-					if(readline.equals("1"))
-					{
+
+					if (readline.equals("1")) {
 						cal = Calendar.getInstance();
 						System.out.println("");
 						System.out.println(sdf.format(cal.getTime()));
-					}
-					else if(readline.equals("2"))
-					{
+					} else if (readline.equals("2")) {
 						System.out.println("-JACSystems 500-");
-					}
-					else if(readline.equals("3"))
-					{
+					} else if (readline.equals("3")) {
+						System.out.println("What do you want to do?");
+						System.out.println("1. See the time");
+						System.out.println("2. What is the OS?");
+						System.out.println("3. Help");
+						System.out.println("4. Exit");
+					} else if (readline.equals("4")) {
 						System.out.println("Goodbye!");
 						exit = true;
-					}
-					else 
-					{
+					} else {
 						System.out.println("invalid choice");
 					}
-					
+					System.out.println("");
 				}
-			}
-			else {
-				
+			} else {
+
 				System.out.println("Incorrect password");
-				
+
 			}
-					
+
 		}
-	
+
 	}
 
 }
