5253f30024e525b3e6821219151ce944691310a6
Jac0blee <jacobleedude@gmail.com>
2012-10-17 11:58:28 -0400

    Simple Mock OS

--- src/com/github/Jac0blee/HelloWorld.java
+++ src/com/github/Jac0blee/HelloWorld.java
@@ -3,6 +3,9 @@ package com.github.Jac0blee;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 
 public final class HelloWorld {
 
@@ -10,9 +13,20 @@ public final class HelloWorld {
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
+
+	
+		        Calendar cal = Calendar.getInstance();
+		        cal.getTime();
+		        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		        
+	
+		boolean exit = false;
+		
 		String readline;
-			System.out.println("Type in the password.");
-		while ((readline = in.readLine()) != null) {
+		
+		System.out.println("Type in the password.");
+		
+		while ((readline = in.readLine()) != null && exit != true) {
 				
 			String password = "Fireworks";
 				
@@ -22,6 +36,35 @@ public final class HelloWorld {
 				
 				System.out.println("Correct password.");
 				
+				while(!exit)
+				{
+					System.out.println("");
+					System.out.println("What do you want to do?");
+					System.out.println("1. See the time");
+					System.out.println("2. What is the OS?");
+					System.out.println("3. Exit");
+					readline = in.readLine();
+					if(readline.equals("1"))
+					{
+						cal = Calendar.getInstance();
+						System.out.println("");
+						System.out.println(sdf.format(cal.getTime()));
+					}
+					else if(readline.equals("2"))
+					{
+						System.out.println("-JACSystems 500-");
+					}
+					else if(readline.equals("3"))
+					{
+						System.out.println("Goodbye!");
+						exit = true;
+					}
+					else 
+					{
+						System.out.println("invalid choice");
+					}
+					
+				}
 			}
 			else {
 				
