059f2364dd35883a7625c8eb67385aa0705f64ff
Jac0blee <jacobleedude@gmail.com>
2012-10-25 15:40:44 -0400

    Working through thinking in java

--- src/com/github/Jac0blee/Thinking_in_Java.java
+++ src/com/github/Jac0blee/Thinking_in_Java.java
@@ -1,6 +1,7 @@
 package com.github.Jac0blee;
 
 import java.io.BufferedReader;
+import java.util.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -9,23 +10,28 @@ public class Thinking_in_Java {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		
-		String readline;
 
-		class Car {
+		String readline;
+		
+		System.out.print("Hello, it's: ");
+		System.out.println(new Date());
+		
+		
+			
+			
+		//this is the classes section :) for some reason im having a hard time figuring out how to 
+		/*class Car {
 
 			int width;
-			int height;
+			int height = 5;
 			int length;
 			String name = "Suzie";
-			
+
 			void ReturnName() {
 				System.out.print("The car's name is: ");
 				System.out.println(name);
-				
-				
+
 			}
-			
 		}
 
 		Car Mustang = new Car();
@@ -33,18 +39,19 @@ public class Thinking_in_Java {
 		Mustang.height = 5;
 		Mustang.width = 8;
 		Mustang.length = 20;
-		
 
 		System.out.println(Mustang.height);
 		System.out.println(Mustang.width);
 		System.out.println(Mustang.length);
 		System.out.println(Mustang.name);
+
+		System.out.println("Choose the name for your mustang!");
 		
 		Mustang.name = in.readLine();
-		
+
 		System.out.println(Mustang.name);
 		Mustang.ReturnName();
-		
+	*/
 	}
 
 }
